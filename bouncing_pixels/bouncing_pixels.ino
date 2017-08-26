204a519ceb004abc000a5bb0bc1d918aef805bbf
Ryan Lett <ryan@eventbase.com>
2017-08-25 22:44:20 -0700

    It works

--- bouncing_pixels/bouncing_pixels.ino
+++ bouncing_pixels/bouncing_pixels.ino
@@ -11,16 +11,16 @@ class BouncingPixel
     double _color;
 
   public:
-    BouncingPixel(int strip_size, double color, int init_pos);
+    BouncingPixel(int strip_size, double color, int init_pos, int dir);
     int next();
     double color();
 };
 
-BouncingPixel::BouncingPixel(int strip_size, double color, int init_pos) {
+BouncingPixel::BouncingPixel(int strip_size, double color, int init_pos, int dir) {
   _color = color;
   _strip_size = strip_size;
   _pos = init_pos;
-  _dir = 1;
+  _dir = dir;
 }
 
 int BouncingPixel::next() {
@@ -41,19 +41,39 @@ double BouncingPixel::color() {
   return _color;
 }
 
-double RED = 0xFF0000;
-double GREEN = 0x800000;
-double BLUE = 0x0000FF;
 
+
+#define NUM_PIXELS 10
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
-double pixelVal[N_LEDS];
-// BouncingPixel greenPixel = BouncingPixel(N_LEDS, 0x00FF00);
-// BouncingPixel redPixel = BouncingPixel(N_LEDS, 0xFF0000, 1);
-BouncingPixel pixels[3] = {
-  BouncingPixel(N_LEDS, RED, 1),
-  BouncingPixel(N_LEDS, GREEN, 6),
-  BouncingPixel(N_LEDS, BLUE, 9)
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+
+BouncingPixel pixels[NUM_PIXELS] = {
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), -1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), 1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), -1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), 1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), -1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), -1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), -1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), -1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), -1),
+  BouncingPixel(N_LEDS, wheel(random(1,255)), random(0, N_LEDS - 1), -1),
 };
+
 
 void setup() {
   strip.begin();
@@ -61,9 +81,9 @@ void setup() {
 
 void loop() {
   int loc;
-  int color;
+  double color;
 
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < NUM_PIXELS; i++) {
     loc = pixels[i].next();
     color = pixels[i].color();
     
